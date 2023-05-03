07ebf0ab1823afdc1eb4d28043cdef3e51874b87
Malcolm Wimbley <malcolmwimbley@gmail.com>
2023-05-02 22:23:22 -0500

    Update Portfolio Component

---
+++ portfolio/src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Teminal Website',
+        imgUrl: '',
+        stack: ['HTML','CSS','Javascript'],
+        link: 'https://wwww.google.com',
+    },
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['HTML','CSS','Javascript'],
+        link: 'https://wwww.google.com',
+    },
+    {
+        title: 'Minecraft',
+        imgUrl: '',
+        stack: ['HTML','CSS','Javascript'],
+        link: 'https://wwww.google.com',
+    },
+]
