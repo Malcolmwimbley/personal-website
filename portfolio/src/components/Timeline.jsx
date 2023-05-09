1152a2fc5907c7553930aacd1996fefa0cb87308
Malcolm Wimbley <malcolmwimbley@gmail.com>
2023-05-09 12:15:00 -0500

    responsive

--- portfolio/src/components/Timeline.jsx
+++ portfolio/src/components/Timeline.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import TimelineItem from "./TimelineItem";
 import timeline from "../data/timeline";
 
-function Timeline(){
+function Timeline() {
     return (
-        <div className="flex flex-col md:flex-row justify-center my-20">
-            <div className="w-full md:w-7/12">
-            {timeline.map(item =>(
-                <TimelineItem
-                    year = {item.year}
-                    title = {item.title}
-                    duration = {item.duration}
-                    details = {item.details}
+       <div className="flex flex-col md:flex-row justify-center my-20">
+          <div className="xl:w-1/4 lg:w-1/2 md:w-7/12 sm:w-full">
+             {timeline.map(item => (
+                <TimelineItem 
+                   year={item.year}
+                   title={item.title}
+                   duration={item.duration}
+                   details={item.details}
                 />
-            ))}
-            </div>
-        </div>
+             ))}
+          </div>
+       </div>
     )
-}
+ }
 
 export default Timeline;
