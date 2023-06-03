2d67459887591f6426573bb932adc024e913f59a
Malcolm Wimbley <malcolmwimbley@gmail.com>
2023-06-03 18:57:04 -0500

    Update Footer.jsx

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer(){
 					<div>
 						<div className="flex flex-wrap justify-center gap-2">
 							<a 
-								href="https://twitter.com/forrestpknight"
+								href="https://twitter.com/malcolm_wimbley"
 								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 								<svg
 									className="w-5 h-5 fill-current"
@@ -19,7 +19,7 @@ function Footer(){
 								</svg>
 							</a>
 							<a 
-								href="https://linkedin.com/in/forrestpknight"
+								href="https://linkedin.com/in/malcolmwimbley"
 								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 								<svg
 								className="w-5 h-5 fill-current"
@@ -44,7 +44,7 @@ function Footer(){
 								</svg>
 							</a>
 							<a 
-								href="https://github.com/forrestknight"
+								href="https://github.com/malcolmwimbley"
 								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 								<svg
 								xmlns="http://www.w3.org/2000/svg"
